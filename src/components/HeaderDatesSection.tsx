833ba5deb0d0494f9286618f536ac89bc8294e59
JBryskiewicz <kuba@fiku.eu>
2024-01-20 14:09:26 +0100

    Fix: Section with dates components now scale properly with screen size

--- src/components/HeaderDatesSection.tsx
+++ src/components/HeaderDatesSection.tsx
@@ -1,5 +1,5 @@
 import { useSelector } from "react-redux";
-import { Chip, Stack } from "@mui/material";
+import { Chip, Grid } from "@mui/material";
 import { applyDate } from "../utils/supportFunctions";
 import Box from "@mui/material/Box";
 import { selectOneSession } from "../redux/sessionSlice";
@@ -23,25 +23,28 @@ export const HeaderDatesSection = ({
   const isEdited = session.edited;
 
   return (
-    <Box sx={{ marginBottom: "1.25rem" }}>
-      <Stack direction="row" spacing={1}>
+    <Box className="date-chip-box">
+      <Grid container item direction="row" xs={1} sm={1} md={12} lg={12}>
         <Chip
           data-testid={plannedDateID}
           label={`planned: ${applyDate(plannedDate)}`}
+          className="date-chip-element"
         />
         <Chip
           data-testid={creationDateID}
           label={`created: ${applyDate(
             isCreatedNull ? new Date().toISOString() : creationDate
           )}`}
+          className="date-chip-element"
         />
         {!isEdited || isCreatedNull ? null : (
           <Chip
             data-testid={editDateID}
             label={`last edited: ${applyDate(editedDate)}`}
+            className="date-chip-element"
           />
         )}
-      </Stack>
+      </Grid>
     </Box>
   );
 };

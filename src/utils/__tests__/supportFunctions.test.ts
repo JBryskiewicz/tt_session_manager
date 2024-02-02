cbd1fb6e50d242970e33a4cf4255300c8112a5c6
JBryskiewicz <kuba@fiku.eu>
2024-02-02 12:58:36 +0100

    checkpoint: coverage for unit tests

--- src/utils/__tests__/supportFunctions.test.ts
+++ src/utils/__tests__/supportFunctions.test.ts
@@ -1,5 +1,8 @@
 import { it, expect, describe } from "vitest";
 import { applyDate } from "../supportFunctions/dateHandlers";
+import { User } from "../../types/types";
+import { MOCK_SESSION_LIST } from "../../test-utils/test-mock-data";
+import { checkUserSessionOwnership } from "../supportFunctions/userHandlers";
 
 describe("Tests for support funtions", () => {
   it("Function returns correctly formated date", () => {
@@ -11,5 +14,22 @@ describe("Tests for support funtions", () => {
   it("Function returns correct value if argument is null", () => {
     const date: string = applyDate(null);
     expect(date).toBe("Session is not planned");
+  });
+
+  it("Returns correct boolean value if user has session included in session list", () => {
+    const user: User = {
+      id: 1,
+      roles: [],
+      email: "test@test.com",
+      sessions: MOCK_SESSION_LIST,
+    };
+    const trueID: number = 999;
+    const falseID: number = 1;
+
+    const resultTrue = checkUserSessionOwnership(user, trueID);
+    expect(resultTrue).toBe(true);
+
+    const resultFalse = checkUserSessionOwnership(user, falseID);
+    expect(resultFalse).toBe(false);
   });
 });

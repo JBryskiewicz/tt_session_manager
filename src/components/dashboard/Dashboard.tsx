fcaf038918ff3c0ef2fec4ef241e2441a150f6f1
JBryskiewicz <kuba@fiku.eu>
2024-02-07 14:07:05 +0100

    revert changes for unathorized fix

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -7,14 +7,9 @@ import { fetchUser, findUser } from "../../redux/userSlice";
 import { auth } from "../../firebase";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { CustomLoading } from "../loaders/CustomLoading";
-import { useNavigate } from "react-router-dom";
-import { ADDRESS_LIB } from "../../utils/libs/constants";
-
-const { login } = ADDRESS_LIB;
 
 export function Dashboard() {
   const dispatch = useAppDispatch();
-  const navigate = useNavigate();
   const [user, loading] = useAuthState(auth);
   const email = user?.email as string;
   const userData = useSelector(findUser);
@@ -25,10 +20,7 @@ export function Dashboard() {
     if (!loading) {
       dispatch(fetchUser({ email }));
     }
-    if (userData.email === "") {
-      navigate(login);
-    }
-  }, [dispatch, email, loading, navigate, userData.email]);
+  }, [dispatch, email, loading]);
 
   if (loading || userData.id === 0) {
     return <CustomLoading />;
--- src/components/session/detailsSession/SessionDetails.tsx
+++ src/components/session/detailsSession/SessionDetails.tsx
@@ -1,12 +1,11 @@
 import { DetailsHeader } from "./DetailsHeader";
 import { useEffect } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { DetailsNotesBody } from "./DetailsNotesBody";
 import { fetchSession } from "../../../redux/sessionSlice";
 import { useAppDispatch, useAppSelector } from "../../../redux/hooks";
 import { ActionButtonSection } from "../../ActionButtonSection";
 import {
-  ADDRESS_LIB,
   ERROR_MESSAGE_LIB,
   SESSION_CATEGORY_LIB,
 } from "../../../utils/libs/constants";
@@ -15,11 +14,11 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../../../firebase";
 import { checkUserSessionOwnership } from "../../../utils/supportFunctions/userHandlers";
 import { CustomLoading } from "../../loaders/CustomLoading";
+
 import { ErrorPageView } from "../../errorPages/ErrorPageView";
 
 const { sessionNotFound } = ERROR_MESSAGE_LIB;
 const { details } = SESSION_CATEGORY_LIB;
-const { login } = ADDRESS_LIB;
 
 type RouteParams = {
   id: string;
@@ -29,7 +28,6 @@ export function SessionDetails() {
   const { id } = useParams<RouteParams>();
   const [user, loading] = useAuthState(auth);
   const dispatch = useAppDispatch();
-  const navigate = useNavigate();
   const userData = useAppSelector(findUser);
 
   const sessionID = parseInt(id as string);
@@ -40,10 +38,7 @@ export function SessionDetails() {
     if (!loading) {
       dispatch(fetchUser({ email }));
     }
-    if (userData.email === "") {
-      navigate(login);
-    }
-  }, [dispatch, id, email, loading, userData.email, navigate]);
+  }, [dispatch, id, email, loading]);
 
   if (loading || !userData) {
     return <CustomLoading />;
